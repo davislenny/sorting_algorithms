6fa8e0843152a938a3f34bef50cf9b05d9a4aa28
davislenny <davidlmwangi&@gmail.com>
2023-01-20 22:23:32 -0800

    Add header

---
+++ sort.h
@@ -0,0 +1,26 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * struct listint_s - DOubly linked list node
+ *
+ * @n: Integer stored in thenode
+ * @prev: Pointer to the previous element of the list
+ * @next: POinter to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
+
+#endif /* SORT_H */

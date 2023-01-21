561e462e4bc00ec5668b9fde2f041a39a9809e58
davislenny <davidlmwangi&@gmail.com>
2023-01-21 02:38:58 -0800

    Add 1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,7 +9,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *temp, *node;
 
-	if (!*list || !(*list)->next)
+	if (!list || !*list || !(*list)->next)
 		return;
 	node = (*list)->next;
 	while (node)

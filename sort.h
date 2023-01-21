23a4148858423a1c9f201ddea5cfe6ae0ea8bb0b
davislenny <davidlmwangi&@gmail.com>
2023-01-21 04:07:03 -0800

    Add header

--- sort.h
+++ sort.h
@@ -26,5 +26,6 @@ listint_t *create_listint(const int *array, size_t size);
 void swap(listint_t *n1, listint_t *n2);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
 
 #endif /* SORT_H */
